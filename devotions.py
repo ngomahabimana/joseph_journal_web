af543f3d826631b82fa62bb8017bbf9db89bc010
ngomahabimana <ngomahabimana@gmail.com>
2025-05-22 23:08:48 -0400

    Update devotions.py

--- devotions.py
+++ devotions.py
@@ -97,4 +97,6 @@ def devotional():
     return render_template("devotional.html", devo=devo, lang=lang)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)), debug=True)
+
